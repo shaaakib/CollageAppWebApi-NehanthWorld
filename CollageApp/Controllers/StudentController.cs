810d0f9b3e98f2af09a2df1b2601bb21a7c206e0
Md Shakib Hossain <shakibhossainmuhammad@gmail.com>
2025-01-16 21:00:20 +0600

    Add project files.

---
+++ CollageApp/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using CollageApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudentName()
+        {
+            return CollageRepository.Students;
+        }
+
+        [HttpGet("{id:int}")]
+        public Student GetStudentById(int id)
+        {
+            return CollageRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+        }
+
+        [HttpGet("{name:alpha}")]
+        public Student GetStudentByName(string name)
+        {
+            return CollageRepository.Students.Where(n => n.StudentName == name).FirstOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public bool StudentByDelete(int id)
+        {
+            var student =  CollageRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            CollageRepository.Students.Remove(student);
+            return true;
+        }
+    }
+}
---
+++ CollageApp/Models/CollageRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace CollageApp.Models
+{
+    public class CollageRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>(){
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Student 1",
+                    Email = "studentemail@gmail.com",
+                    Address = "Hyd, India"
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Student 2",
+                    Email = "studentemail2@gmail.com",
+                    Address = "Hyd, BD"
+                },
+            };
+    }
+}
